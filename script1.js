dcf87306cc270d767b875642c3bcc78a838d99dc
OksanaGlobazh-1991 <oksanaglobaz9@gmail.com>
2021-08-04 01:21:05 +0300

    lesson03

--- script1.js
+++ script1.js
@@ -1,7 +1,43 @@
-let num = 266219;
-let arr = Array.from(String(num), Number);
-let str = (arr.reduce((sum, current) => sum * current));
-let result = str ** 3;
+// через if
+let lang = prompt('Выберите язык!', 'ru or en');
 
-console.log(str);
-console.log(String(result).substr(0,2));
+let arrRussia = ['Понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+let arrEnglish = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+
+if (lang === 'ru') {
+  console.log(arrRussia);
+} else if (lang === 'en') {
+  console.log(arrEnglish);
+} else {
+  console.log('упс');
+};
+
+// через switch
+switch (lang) {
+  case 'ru':
+    console.log(arrRussia);
+    break;
+  case 'en':
+    console.log(arrEnglish);
+    break;
+  default:
+    console.log('упс');
+}
+
+// через многомерный массив
+let arr = {
+  'ru':['Понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
+  'en':['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+}
+
+console.log(arr.ru);
+console.log(arr.en);
+
+// 2-ое задание
+
+let nameYou = prompt('Ваше имя?', 'Артем или Максим');
+let namePerson = (nameYou === 'Артем') ? 'директор' : 
+    (nameYou === 'Максим') ? 'преподаватель' : 'студент';
+  
+console.log(namePerson);    
